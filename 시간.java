df1c6e1a556e62608b4ffd6888d0e033d3e6381f
Haydnkr <165639764+Haydnkr@users.noreply.github.com>
2024-06-06 16:47:51 +0900

    Update 시간.java

--- 시간.java
+++ 시간.java
@@ -4,12 +4,25 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
-
+import javax.swing.plaf.FontUIResource;
 import java.awt.*;
 
 public class Sec {
+	public static void setUIFont(FontUIResource f) {
+		
+        java.util.Enumeration<Object> keys = UIManager.getDefaults().keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get(key);
+            if (value instanceof FontUIResource) {
+                UIManager.put(key, f);
+            }
+        }
+    }
 	public static void main(String[] args)
 	{
+		setUIFont(new FontUIResource(new Font("Malgun Gothic", Font.PLAIN, 16)));
+		// 글씨 크기, 체 변경
 		Dimension dim = new Dimension(1920,1080);
 		JFrame frame = new JFrame("캘린더");
 		frame.setLocation(100,100);
@@ -52,5 +65,55 @@ public class Sec {
 		frame.setVisible(true);	
 		
 	}
+//맑은 고딕 사용하기
+}
 
+class CheckBoxRenderer extends JCheckBox implements TableCellRenderer {
+    public CheckBoxRenderer() {
+        setHorizontalAlignment(JLabel.CENTER);
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        if (isSelected) {
+            setBackground(table.getSelectionBackground());
+            setForeground(table.getSelectionForeground());
+                        
+        } else {
+            setBackground(table.getBackground());
+            setForeground(table.getForeground());
+        }
+        setSelected((value != null && (Boolean) value));
+        return this;
+    }
+}
+
+class StrikethroughRenderer extends DefaultTableCellRenderer {
+    private JLabel label; 
+
+    public StrikethroughRenderer() {
+        label = new JLabel(); 
+        label.setOpaque(true); 
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        boolean isCompleted = (Boolean) table.getValueAt(row, 0);
+
+        if (isSelected) { 
+            label.setBackground(table.getSelectionBackground()); 
+            label.setForeground(table.getSelectionForeground());
+        } else { 
+            label.setBackground(table.getBackground());
+            label.setForeground(table.getForeground()); 
+        }
+
+        if (isCompleted) { 
+            label.setText("<html><strike>" + value + "</strike></html>");
+        } else {
+            label.setText("<html>" + value + "</html>");
+        }
+
+        return label;
+    }
 }
